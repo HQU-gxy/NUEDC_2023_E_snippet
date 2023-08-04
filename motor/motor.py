005295f228ef7076d855408dd4d2fb27976678dd
crosstyan <crosstyan@outlook.com>
2023-08-05 03:39:12 +0800

    refactor motor, video works

---
+++ motor/motor.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+from .pkt import *
+from typing import Optional
+from .protocol import MotorProtocol, Direction
+from .mapper import PiecewiseLinearParams, ExtremumParams, piecewise_linear_mapper
+from .utils import Instance
+from loguru import logger
+import asyncio
+
+
+@dataclass
+class Motion:
+    speed: float
+    direction: Direction
+
+
+def reverse_direction(direction: Direction):
+    return Direction.CCW if direction == Direction.CW else Direction.CW
+
+
+class Motor:
+    id: int
+    protocol: MotorProtocol
+    last_motion: Optional[Motion]
+    last_position_deg: float
+    last_write: Instance
+    spd_ex: ExtremumParams
+    spd_piece: PiecewiseLinearParams
+    delay_ex: ExtremumParams
+    delay_piece: PiecewiseLinearParams
+
+    def __init__(self, id: int, protocol: MotorProtocol) -> None:
+        MAX_POS_ABS = 18
+        self.id = id
+        self.protocol = protocol
+        self.last_motion = None
+        self.last_position_deg = 0.0
+        self.last_write = Instance()
+        self.spd_ex = ExtremumParams(0, MAX_POS_ABS, 0, 10)
+        self.spd_piece = PiecewiseLinearParams(10, 0.5, 0.8, 2.5)
+        self.delay_ex = ExtremumParams(0, MAX_POS_ABS, 0.005, 0.05)
+        self.delay_piece = PiecewiseLinearParams(10, 0.5, 0.8, 2.5)
+
+    async def to_degree(self, degree: float, precision: float = 0.6):
+        # 5ms
+        D = 0.005
+
+        def is_in_range(pos: int | float, target: int | float, err: int | float):
+            return abs(pos - target) < err
+
+        def pos_spd_map(pos): return piecewise_linear_mapper(
+            pos, self.spd_ex, self.spd_piece)
+        def pos_delay_map(pos): return piecewise_linear_mapper(
+            pos, self.delay_ex, self.delay_piece)
+
+        self.last_position_deg = await self.protocol.read_position_deg(self.id)
+        spd = pos_spd_map(self.last_position_deg)
+        await asyncio.sleep(D)
+        while True:
+            assert isinstance(self.last_position_deg, float)
+            if is_in_range(self.last_position_deg, degree, precision):
+                break
+            diff = degree - self.last_position_deg
+            direction: Direction
+            if diff > degree:
+                direction = Direction.CCW
+            else:
+                direction = Direction.CW
+            spd = pos_spd_map(self.last_position_deg)
+            self.protocol.ctrl_speed(self.id, direction, int(spd))
+            self.last_motion = Motion(spd, direction)
+            await asyncio.sleep(pos_delay_map(self.last_position_deg))
+            self.last_position_deg = await self.protocol.read_position_deg(self.id)
+            logger.debug("pos: {}".format(self.last_position_deg))
+        self.protocol.ctrl_stop(self.id)
+
+    async def delta_degree(self, delta: float):
+        deg = await self.protocol.read_position_deg(self.id)
+        self.last_position_deg = deg
+        assert isinstance(self.last_position_deg, float)
+        await self.to_degree(self.last_position_deg + delta)
+
+
+class RotateTiltMotor:
+    rotate_motor: Motor
+    tilt_motor: Motor
+
+    def __init__(self, rotate_motor: Motor, tilt_motor: Motor) -> None:
+        self.rotate_motor = rotate_motor
+        self.tilt_motor = tilt_motor
+
+    # https://stackoverflow.com/questions/34377319/combine-awaitables-like-promise-all
+    async def delta_degree(self, rotate_delta: float, tilt_delta: float):
+        await asyncio.gather(
+            self.rotate_motor.delta_degree(rotate_delta),
+            self.tilt_motor.delta_degree(tilt_delta)
+        )
--- video.py
+++ video.py
@@ -24,9 +24,13 @@ class Instance:
         elapsed = self.elapsed()
         self.reset()
         return elapsed
+
+
 Mat = cv2.Mat
 
 # preprocess image with color range
+
+
 def extractByThresh(image: Mat,
                     color_min: Tuple[int, int, int],
                     color_max: Tuple[int, int, int],
@@ -43,34 +47,38 @@ def extractByThresh(image: Mat,
     blur_again = cv2.GaussianBlur(lab_red, (5, 5), 2, 2)
     return blur_again
 
-def handle_frame(frame:cv2.Mat) -> cv2.Mat:
-  color_min_red = (10, 144, 121)
-  color_max_red = (245, 255, 152)
-  color_min_green = (18, 60, 135)
-  color_max_green = (245, 108, 255)
-  crop_frame = frame[0:550, 170:740]
-  binary = extractByThresh(crop_frame, color_min_red, color_max_red) # type: ignore
-  # https://docs.opencv.org/3.4/d0/d7a/classcv_1_1SimpleBlobDetector.html
-  params = cv2.SimpleBlobDetector_Params()
-  from math import pi
-  params.filterByCircularity = False
-  params.filterByConvexity = False
-  params.filterByInertia = False
-  params.filterByColor = False
-  params.filterByArea = True
-  params.minArea = pi * 1 ** 2
-  params.maxArea = pi * 10 ** 2
-  blob = cv2.SimpleBlobDetector_create(params)
-  # blob.filterByArea = True
-  # https://stackoverflow.com/questions/20466676/simpleblobdetector-filtering-by-area
-  kps = blob.detect(binary)
-  for kp in kps:
-    x, y = kp.pt
-    cv2.circle(crop_frame, (int(x), int(y)), 10, (0, 0, 255), -1)
-    logger.info(f"Found blob at {x}, {y}")
 
-  ret = crop_frame
-  return ret # type: ignore
+def handle_frame(frame: cv2.Mat) -> cv2.Mat:
+    color_min_red = (10, 144, 121)
+    color_max_red = (245, 255, 152)
+    color_min_green = (18, 60, 135)
+    color_max_green = (245, 108, 255)
+    crop_frame = frame[0:550, 170:740]
+    binary = extractByThresh(crop_frame, color_min_red,
+                             color_max_red)  # type: ignore
+    # https://docs.opencv.org/3.4/d0/d7a/classcv_1_1SimpleBlobDetector.html
+    params = cv2.SimpleBlobDetector_Params()
+    from math import pi
+    # https://www.geeksforgeeks.org/find-circles-and-ellipses-in-an-image-using-opencv-python/
+    params.filterByCircularity = False
+    params.filterByConvexity = False
+    params.filterByInertia = False
+    params.filterByColor = False
+    params.filterByArea = True
+    params.minArea = pi * 1 ** 2
+    params.maxArea = pi * 10 ** 2
+    blob = cv2.SimpleBlobDetector_create(params)
+    # blob.filterByArea = True
+    # https://stackoverflow.com/questions/20466676/simpleblobdetector-filtering-by-area
+    kps = blob.detect(binary)
+    for kp in kps:
+        x, y = kp.pt
+        cv2.circle(crop_frame, (int(x), int(y)), 10, (0, 0, 255), -1)
+        logger.info(f"Found blob at {x}, {y}")
+
+    ret = crop_frame
+    return ret  # type: ignore
+
 
 async def runVideo():
     frame_instance = Instance()
@@ -83,7 +91,7 @@ async def runVideo():
             r = handle_frame(frame)
             cv.imshow("frame", r)
             if cv.waitKey(1) & 0xFF == ord('q'):
-              exit()
+                exit()
             elapsed = frame_instance.elapsed()
             diff = duration_ms - elapsed
             if diff < 0:
@@ -92,8 +100,8 @@ async def runVideo():
             await asyncio.sleep(diff / 1000)
             frame_instance.reset()
         else:
-          logger.warning("No frame")
-          exit()
+            logger.warning("No frame")
+            exit()
 
 
 def main():
@@ -106,6 +114,7 @@ def main():
 # https://docs.opencv.org/3.4/d0/d7a/classcv_1_1SimpleBlobDetector.html
 # https://learnopencv.com/blob-detection-using-opencv-python-c/
 # https://docs.arduino.cc/tutorials/nicla-vision/blob-detection
+
 
 if __name__ == "__main__":
     main()
